bf5a5544ef93371b6be05ca414ed3bd8ec44984c
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-01-09 14:02:58 +0100

    Matchpagina, switchen van matches werkend maken
    Ook index.js verwijderd omdat we hem niet gebruiken, de over ons pagina iets aangepast, en de inlogvelden een groen balkje gegeven bij juiste gegevens

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,13 +1,56 @@
-/* Controleert of gebruikersnaam en wachtwoord goed zijn,
-*  bij correcte invoer kom je op de ingelogde omgeving en krijg je een melding dat het gelukt is,
-*  bij foutieve invoer krijg je een foutmelding.
+/* Deze functie checkt of de ingevoerde gegevens van de inputvelden juist zijn en geeft het veld een rode of groene kleur, en eventueel een foutmelding
+*  De functie wordt aangeroepen door het onblur event, dus op het moment dat je van het huidige invoerveld af gaat.
+*/
+function check() {
+
+    let gebruikersnaamVeld = document.getElementById("inputEmail");
+    let gebruikersnaamValue = gebruikersnaamVeld.value;
+    let wachtwoordVeld = document.getElementById("inputPassword");
+    let wachtwoordValue = wachtwoordVeld.value;
+
+    /* checkt of de gebruikersnaam in de database voorkomt, maar alleen als er iets in het veld is ingevuld */
+    if (gebruikersnaamValue !== "") {
+        FYSCloud.API.queryDatabase(
+            "SELECT email FROM profiel WHERE email = BINARY ?", [gebruikersnaamValue] //BINARY zorgt ervoor dat de zoekterm case sensitive is
+        ).done(function (data) {
+            if (data.length > 0) {
+                gebruikersnaamVeld.classList.add("inputGood");
+                document.getElementById("errorveld3").classList.add("verborgen");
+            } else {
+                gebruikersnaamVeld.classList.add("inputError");
+                document.getElementById("errorveld3").classList.remove("verborgen");
+            }
+        }).fail(function (data) {
+            console.log(data);
+        });
+    };
+
+    /* checkt of zowel de gebruikersnaam als het wachtwoord overeenkomen, maar alleen als beide velden zijn ingevuld */
+    if (gebruikersnaamValue !== "" && wachtwoordValue !== "") {
+        FYSCloud.API.queryDatabase(
+            "SELECT email, wachtwoord FROM profiel WHERE email = BINARY ? AND wachtwoord = BINARY ? ", [gebruikersnaamValue, wachtwoordValue]
+        ).done(function (data) {
+            if (data.length > 0) {
+                gebruikersnaamVeld.classList.add("inputGood");
+                wachtwoordVeld.classList.add("inputGood");
+                document.getElementById("errorveld3").classList.add("verborgen");
+                document.getElementById("errorveld4").classList.add("verborgen");
+                document.getElementById("errorveld5").classList.add("verborgen");
+            }
+        }).fail(function (data) {
+            console.log(data);
+        });
+    };
+};
+
+/* Deze functie wordt aangeroepen als je op de log in knop klikt en controleert of gebruikersnaam en wachtwoord goed zijn,
+*  bij correcte invoer kom je op de ingelogde omgeving, bij foutieve invoer krijg je een foutmelding.
 */
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
     loginButton.onclick = function () {
 
-        let userid = FYSCloud.Session.get("userid");
         let gebruikersnaamveld = document.getElementById("inputEmail");
         let gebruikersnaam = gebruikersnaamveld.value;
         let wachtwoordveld = document.getElementById("inputPassword");
@@ -25,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
         ).done(function (data) {
             if (data.length > 0) {
                 FYSCloud.Session.set("userid", data[0]["id"]);
-                window.location.href = 'match.html';
+                FYSCloud.URL.redirect("match.html");
             } else {
                 if (gebruikersnaam == "" && wachtwoord == "") {
                     gebruikersnaamveld.classList.add("inputError");
@@ -34,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
                 } else if (wachtwoord == "") {
                     wachtwoordveld.classList.add("inputError");
                     document.getElementById("errorveld5").classList.remove("verborgen");
-                } else if (gebruikersnaam == ""){
+                } else if (gebruikersnaam == "") {
                     gebruikersnaamveld.classList.add("inputError");
                     document.getElementById("errorveld5").classList.remove("verborgen");
                 } else {
