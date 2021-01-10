3d1970db69f1b4de527630f4771029fab6f798a9
Sarah Chrzanowska-Buth <sarah.chrzanowska-buth@hva.nl>
2021-01-10 22:22:43 +0100

    Registreer pagina

--- assets/js/pages/registreer.js
+++ assets/js/pages/registreer.js
@@ -16,7 +16,7 @@ function firstNameCheck() {
 }
 
 function lastNameCheck() {
-    let lastNameExp = new RegExp( /^([^0-9!@#$%^&§€*]*)$/);
+    let lastNameExp = new RegExp(/^([^0-9!@#$%^&§€*]*)$/);
     let uncheckedLastName = document.getElementById("lastName");
     let uncheckedLastNameValue = uncheckedLastName.value;
 
@@ -50,16 +50,16 @@ function budgetCheck() {
 }
 
 function passwordCheck() {
-    let passwordExp = new RegExp(/([a-z.!0-9@#$%^&*_+()\d]{8,20})?$/i);
+    let passwordExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,20})");
     let uncheckedPassword = document.getElementById("user-password");
     let uncheckedPasswordValue = uncheckedPassword.value;
 
     if (uncheckedPasswordValue !== "") {
         if (passwordExp.test(uncheckedPasswordValue)) {
             uncheckedPassword.classList.remove("inputError");
-            document.getElementById("error-password").classList.add("onzichtbaar");
+            document.getElementById("error-password").classList.add("onzichtbaar-2");
         } else {
-            document.getElementById("error-password").classList.remove("onzichtbaar");
+            document.getElementById("error-password").classList.remove("onzichtbaar-2");
             uncheckedPassword.classList.add("inputError");
         }
         return false;
@@ -93,6 +93,7 @@ function checkEmailAvailable() {
             "SELECT email FROM profiel WHERE email = ?", [uncheckedEmailValue]
         ).done(function (data) {
             if (data.length > 0) {
+                console.log("dit email bestaat al");
                 uncheckedEmail.classList.add("inputError");
                 document.getElementById("email-availability").classList.remove("onzichtbaar-1");
             } else {
@@ -118,150 +119,141 @@ function bioCheck() {
     }
 }
 
+function privacyCheck(){
+    let privacy = document.getElementById("privacy");
+    let privacyVeld = document.getElementById("error-privacy");
+    if (privacy.checked === false) {
+        privacyVeld.classList.remove("onzichtbaar");
+    } else {
+        privacyVeld.classList.add("onzichtbaar");
+    }
+}
+
 $(document).ready(function () {
-    const alertbox = $('.alertbox');
-    const alertbox1 = $('.alertbox1');
-    const alertbox2 = $('.alertbox2');
-    let on = $("#registreren").on("click", function () {
+    $("#registreren").on("click", function () {
+        const alertbox = $('.alertbox');
+        let insertId = 0;
+        let firstName = document.getElementById("firstName").value;
+        let lastName = document.getElementById("lastName").value;
+        let gender = document.getElementById("gender").value;
+        let woonplaats = document.getElementById("woonplaats").value;
+        let budget = document.getElementById("budget").value;
+        let reisbestemming = document.getElementById("reisbestemming").value;
+        let gebruikersnaam = document.getElementById("email").value;
+        let wachtwoord = document.getElementById("user-password").value;
+        let geboortedatum = document.getElementById("geboortedatum").value;
+        let bio = document.getElementById("bio").value;
+        let profielfoto = document.getElementById("profielfoto").value;
         let privacy = document.getElementById("privacy");
-        if ((privacy).checked) {
-            // Slaat de door de gebruiker ingevoerde waarde voor 'VOORNAAM' op in de onderstaande variabele
-            let firstName = document.getElementById("firstName").value;
+        let privacyVeld = document.getElementById("error-privacy");
 
-            let insertId = 0;
+        if (firstName === "" || lastName === "" || profielfoto === "" || gender === "" || woonplaats === "" || budget === "" || reisbestemming === "" ||
+            gebruikersnaam === "" || wachtwoord === "" || bio === "" || geboortedatum === "") {
 
-            // Slaat de door de gebruiker ingevoerde waarde voor 'ACHTERNAAM' op in de onderstaande variabele
-            let lastName = document.getElementById("lastName").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'GENDER' op in de onderstaande variabele
-            let gender = document.getElementById("gender").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'WOONPLAATS' op in de onderstaande variabele
-            let woonplaats = document.getElementById("woonplaats").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'REISBUDGET' op in de onderstaande variabele
-            let budget = document.getElementById("budget").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'REISBESTEMMING' op in de onderstaande variabele
-            let reisbestemming = document.getElementById("reisbestemming").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'E-MAIL' op in de onderstaande variabele
-            let gebruikersnaam = document.getElementById("email").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'WACHTWOORD' op in de onderstaande variabele
-            let wachtwoord = document.getElementById("user-password").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'GEBOORTEDATUM' op in de onderstaande variabele
-            let geboortedatum = document.getElementById("geboortedatum").value;
-
-            // Slaat de door de gebruiker ingevoerde waarde voor 'BIOGRAFIE' op in de onderstaande variabele
-            let bio = document.getElementById("bio").value;
-
-            /* Voornaam, achternaam, gender, woonplaats, budget, reisbestemming, e-mail, wachtwoord, biografie
-            en geboortedatum uploaden naar de database binnen een specifiek profiel mits de velden niet leeg zijn*/
-            if (firstName !== "" && lastName !== "" && woonplaats !== "" && budget !== "" && reisbestemming !== "" &&
-                gebruikersnaam !== "" && wachtwoord !== "" && bio !== "" && geboortedatum !== "") {
-                FYSCloud.API.queryDatabase(
-                    "INSERT INTO profiel(voornaam,achternaam,gender,woonplaats,budget,reisbestemming,email,wachtwoord,bio,geboortedatum)" +
-                    "values(?,?,?,?,?,?,?,?,?,?)",
-                    [firstName, lastName, gender, woonplaats, budget, reisbestemming, gebruikersnaam, wachtwoord, bio, geboortedatum]
-                ).done(function (data) {
-                    insertId = data["insertId"];
-                    insertInteresse(insertId);
-                });
-            } else {
-                alertbox.children().remove();
-                alertbox.append(
-                    `<div class="alert alert-danger" role="alert">
+            alertbox.children().remove();
+            alertbox.append(
+                `<div class="alert alert-danger" role="alert">
                     Er zijn nog lege velden. Controleer uw registratie.       
                     </div>`);
-                window.scroll({
-                    top: 0,
-                    left: 0,
-                    behavior: 'smooth'
-                });
-            }
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: 'smooth'
+            });
+        } else {
+            alertbox.children().remove()
 
-            /* Profielfoto uploaden naar de database binnen een specifiek profiel en de gebruiker na het invullen
-            van het registratieformulier redirecten naar de inlogpagina mits het veld niet leeg is */
-            let profielfoto = document.getElementById("profielfoto");
-            if (profielfoto !== null) {
-                FYSCloud.Utils
-                    .getDataUrl($("#profielfoto"))
-                    .done(function (data) {
-                        console.log(data["extension"]);
-                        FYSCloud.API.uploadFile(
-                            insertId + 'profielfoto.' + data["extension"],
-                            data.url,
-                            true
-                        ).done(function (data) {
-                            console.log(data);
-                            FYSCloud.API.queryDatabase(
-                                "UPDATE profiel SET profielfoto = ? where id = ?", [data, insertId]
-                            ).done(function (data) {
-                                let insertId = data["insertId"];
-                                window.setTimeout(function () {
-                                    FYSCloud.URL.redirect('login.html');
-                                }, 2000);
-                                alertbox1.append(
-                                    `<div class="alert alert-success" role="alert">
-                                Gefeliciteerd, uw registratie is gelukt.       
+            //Checkt of privacy is aangevinkt
+            if (privacy.checked === false) {
+                privacyVeld.classList.remove("onzichtbaar");
+            } else {
+                privacyVeld.classList.add("onzichtbaar");
+                send();
+            }
+        }
+    });
+});
+
+let insertId;
+
+function send() {
+    /* Voornaam, achternaam, gender, woonplaats, budget, reisbestemming, e-mail, wachtwoord, biografie
+    en geboortedatum uploaden naar de database binnen een specifiek profiel mits de velden niet leeg zijn*/
+    let firstName = document.getElementById("firstName").value;
+    let lastName = document.getElementById("lastName").value;
+    let gender = document.getElementById("gender").value;
+    let woonplaats = document.getElementById("woonplaats").value;
+    let budget = document.getElementById("budget").value;
+    let reisbestemming = document.getElementById("reisbestemming").value;
+    let gebruikersnaam = document.getElementById("email").value;
+    let wachtwoord = document.getElementById("user-password").value;
+    let geboortedatum = document.getElementById("geboortedatum").value;
+    let bio = document.getElementById("bio").value;
+
+    FYSCloud.API.queryDatabase(
+        "INSERT INTO profiel(voornaam,achternaam,gender,woonplaats,budget,reisbestemming,email,wachtwoord,bio,geboortedatum)" +
+        "values(?,?,?,?,?,?,?,?,?,?)",
+        [firstName, lastName, gender, woonplaats, budget, reisbestemming, gebruikersnaam, wachtwoord, bio, geboortedatum]
+    ).done(function (data) {
+        insertId = data["insertId"];
+        insertInteresse(insertId);
+    }).fail(function (reason) {
+        console.log(reason);
+    });
+
+    /* Profielfoto uploaden naar de database binnen een specifiek profiel en de gebruiker na het invullen
+    van het registratieformulier redirecten naar de inlogpagina mits het veld niet leeg is */
+    FYSCloud.Utils
+        .getDataUrl($("#profielfoto"))
+        .done(function (data) {
+            console.log(data["extension"]);
+            FYSCloud.API.uploadFile(
+                insertId + 'profielfoto.' + data["extension"],
+                data.url,
+                true
+            ).done(function (data) {
+                console.log(data);
+                FYSCloud.API.queryDatabase(
+                    "UPDATE profiel SET profielfoto = ? where id = ?", [data, insertId]
+                ).done(function (data) {
+                    let insertId = data["insertId"];
+                    window.setTimeout(function () {
+                        FYSCloud.URL.redirect('login.html');
+                    }, 3000);
+                    const alertbox1 = $('.alertbox1');
+                    alertbox1.append(
+                        `<div class="alert alert-success" role="alert">
+                                Gefeliciteerd, uw registratie is gelukt.
                                 </div>`);
-                                window.scroll({
-                                    top: 0,
-                                    left: 0,
-                                    behavior: 'smooth'
-                                });
-                            }).fail(function (data) {
-                                console.log(data);
-                            });
-                        }).fail(function () {
-                            alertbox2.children().remove();
-                            alertbox2.append(
-                                `<div class="alert alert-danger" role="alert">
-                        Upload een profielfoto      
-                        </div>`);
-                            window.scroll({
-                                top: 0,
-                                left: 0,
-                                behavior: 'smooth'
-                            });
-                        });
-                    }).fail(function () {
-                    alertbox2.children().remove();
-                    alertbox2.append(
-                        `<div class="alert alert-danger" role="alert">
-                        Upload een profielfoto      
-                        </div>`);
                     window.scroll({
                         top: 0,
                         left: 0,
                         behavior: 'smooth'
                     });
+                }).fail(function (data) {
+                    console.log(data);
                 });
-            } else {
-                alert("Er is geen foto geüpload!")
-            }
-
-            /* Interesses/hobby's uploaden naar de database */
-            function insertInteresse(insertId) {
-
-                let interesses = document.querySelectorAll('#multiple-checkboxes option:checked');
-                const values = Array.from(interesses).map(el => el.value);
-                console.log(values);
-
-                for (let i = 0; i < values.length; i++) {
-                    let interesses = values[i];
-                    FYSCloud.API.queryDatabase("INSERT INTO profiel_has_interesse (Profiel_id, Interesse_id) VALUES (?, ?)", [insertId, interesses])
-                        .done(function (data) {
-                        }).fail(function (data) {
-                        console.log(data);
-                    });
-                }
-            }
-        } else {
-            console.log("helaas");
-        }
+            }).fail(function (data) {
+                console.log(data);
+            });
+        }).fail(function (data) {
+        console.log(data);
     });
-});
 
+    /* Interesses/hobby's uploaden naar de database */
+    function insertInteresse(insertId) {
+
+        let interesses = document.querySelectorAll('#multiple-checkboxes option:checked');
+        const values = Array.from(interesses).map(el => el.value);
+        console.log(values);
+
+        for (let i = 0; i < values.length; i++) {
+            let interesses = values[i];
+            FYSCloud.API.queryDatabase("INSERT INTO profiel_has_interesse (Profiel_id, Interesse_id) VALUES (?, ?)", [insertId, interesses])
+                .done(function (data) {
+                }).fail(function (data) {
+                console.log(data);
+            });
+        }
+    }
+}
